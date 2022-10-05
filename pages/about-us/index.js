1a259d0461fbd103d4d60e17ace3e538040baf85
Abdurrahman Fawaz <inifawaz@gmail.com>
2022-10-05 23:35:41 +0700

    afafd

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 
 export default function Index() {
     const router = useRouter();
+    console.log("test");
     return (
         <Layout>
             <Container>
